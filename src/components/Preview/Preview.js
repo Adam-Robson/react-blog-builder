7534ffb7a31f69cfaaf9a7ebc15d8529c1c5f0b0
Adam Robson <adamray312@gmail.com>
2022-09-22 22:36:39 -0700

    integrate useState for title and setTitle

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,18 +1,16 @@
-import React from 'react';
-
 import './Preview.css';
+import '../Home/Home';
 
-export default function Preview() {
-  // implement a Preview screen here
-  //  Note - the HTML should have the following structure
-  //  the main div should have a class of preview and the font-name prop
-  //  and should have style interpolated using the alignment prop
+export default function Preview({ title }) {
 
-  // <div class="preview indie-flower" style="text-align: center;">
-  //   <h1> Blog Title</h1>
-  //   <h3> Blog Subtitle</h3>
-  //   <p>This is some blog text. </p>
-  // </div>
 
-  return <></>;
+
+  return (
+    <div>
+      {/* <div className="preview" style="text-align: center;"> */}
+      <h1>{ title }</h1>
+      <h3>Blog Subtitle</h3>
+      <p>This is some blog text. </p>
+    </div>
+  );
 }
